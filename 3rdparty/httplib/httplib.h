c49f9009390abc6932d402d92b25237695e89402
U-vvv-PC\vvv <singe_horizontal@protonmail.com>
2024-01-11 00:35:18 +0100

    following up

--- 3rdparty/httplib/httplib.h
+++ 3rdparty/httplib/httplib.h
@@ -1549,7 +1549,7 @@ inline uint64_t get_header_value<uint64_t>(const Headers &headers,
   auto it = rng.first;
   std::advance(it, static_cast<ssize_t>(id));
   if (it != rng.second) {
-    return std::strtoull(it->second.data(), nullptr, 10);
+    return strtoull(it->second.data(), nullptr, 10);
   }
   return def;
 }
